a01654fb138157a40b625e787795782e67229eb7
benlittlenz <ben@pop-os.localdomain>
2020-09-28 19:56:06 +1300

    inital commit

---
+++ resources/views/livewire/datatable.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <table class="table table-striped">
+        <thead>
+
+        </thead>
+
+        <tbody>
+            @foreach($this->records() as $record)
+                <tr>
+                    <td>
+                        {{ $record->id}}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
