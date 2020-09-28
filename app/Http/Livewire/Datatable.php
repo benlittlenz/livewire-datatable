6b94f9884cdedd091fdb9ce0384b7ef9ac55a9ed
benlittlenz <ben@pop-os.localdomain>
2020-09-28 20:25:01 +1300

    added methods to fetch columns from builder query

--- app/Http/Livewire/Datatable.php
+++ app/Http/Livewire/Datatable.php
@@ -3,14 +3,22 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Schema;
 
 class Datatable extends Component
 {
     public $model;
+    public $columns;
 
     public function mount($model)
     {
         $this->model = $model;
+        $this->columns = $this->columns();
+    }
+
+    public function columns()
+    {
+        return Schema::getColumnListing($this->builder()->getQuery()->from);
     }
 
     public function builder()
@@ -22,7 +30,7 @@ class Datatable extends Component
     {
         return $this->builder()->get();
     }
-    
+
     public function render()
     {
         return view('livewire.datatable');
